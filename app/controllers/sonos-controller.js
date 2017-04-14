fb2eaa10b05589552029684e04cbf840906b9697
Nathan Cashmore <nathan.cashmore@staticvoid.co.uk>
2017-04-14 22:17:57 +0100

    Add POST method to accept Api.Ai input

--- app/controllers/sonos-controller.js
+++ app/controllers/sonos-controller.js
@@ -4,9 +4,15 @@ const logger = require('winston');
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-  logger.log('info', res.data);
-
   res.json({ status: 'ok' });
+});
+
+router.post('/', (req, res) => {
+  const song = req.body.result.parameters['music-song'];
+
+  logger.log('info', `COMMAND will play song ${song}`);
+
+  res.json({ status: 'ok', song});
 });
 
 module.exports = router;
