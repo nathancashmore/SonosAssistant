bb327be2bdca2d555544bd46e30267f11484fd69
Nate <nathan.cashmore@staticvoid.co.uk>
2017-04-15 13:54:47 +0100

    Add linting, reformat error handling and refactor response handling (#3)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SonosAssistant
 Intermediate service to receive Google Home request and interpret to play tunes on Sonos
+
+# Overview
+
+
+
--- test/unit/sonos.spec.js
+++ test/unit/sonos.spec.js
@@ -1,4 +1,3 @@
-const helper = require('./../spec-helper');
 const request = require('request-promise-native');
 const mocha = require('mocha');
 const expect = require('chai').expect;
@@ -8,41 +7,40 @@ const it = mocha.it;
 
 const playSongInputJson = require('../data/play-song.json');
 const playSongOutputJson = require('../data/play-song-response.json');
+const statusOkOutputJson = require('../data/status-ok-response.json');
 
 describe('Sonos controller', () => {
-
   it('should be successful when using GET', (done) => {
     const getRequest = {
       uri: 'http://localhost:3000/sonos/',
       qs: {
-        access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
+        access_token: 'xxxxx xxxxx', // -> uri + '?access_token=xxxxx%20xxxxx'
       },
       headers: {
-        'User-Agent': 'Request-Promise'
+        'User-Agent': 'Request-Promise',
       },
-      json: true // Automatically parses the JSON string in the response
+      json: true, // Automatically parses the JSON string in the response
     };
 
     request(getRequest)
-      .then((body) => {
-        expect(body).to.deep.equal({status: 'ok'});
-        done()
-      })
+    .then((body) => {
+      expect(body).to.deep.equal(statusOkOutputJson);
+      done();
+    });
   });
 
   it('should be successful when using POST', (done) => {
-      const postRequest = {
-        method: 'POST',
-        uri: 'http://localhost:3000/sonos/',
-        body: playSongInputJson,
-        json: true // Automatically stringifies the body to JSON
-      };
+    const postRequest = {
+      method: 'POST',
+      uri: 'http://localhost:3000/sonos/',
+      body: playSongInputJson,
+      json: true, // Automatically stringifies the body to JSON
+    };
 
-      request(postRequest)
-        .then((body) => {
-          expect(body).to.deep.equal(playSongOutputJson);
-          done()
-        })
-    }
-  );
+    request(postRequest)
+      .then((body) => {
+        expect(body).to.deep.equal(playSongOutputJson);
+        done();
+      });
+  });
 });
